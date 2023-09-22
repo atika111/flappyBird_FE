e8b0dd47957ef09208381a8cdd483a2eb63a10d5
evgenyrinkevich <rinkevich2000@gmail.com>
2023-09-22 14:38:22 +0300

    fixing errors from the server on signup

--- src/Account/Signup.jsx
+++ src/Account/Signup.jsx
@@ -1,64 +1,64 @@
-import React, { useReducer,} from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import axios from "axios";
-import UploadImage from "./UploadImage";
-import { userReducer, initialState } from "../../Reducers/userReducer";
+import React, { useReducer } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import axios from 'axios';
+import UploadImage from './UploadImage';
+import { userReducer, initialState } from '../../Reducers/userReducer';
 
 const Signup = ({ openSignup, onClose }) => {
   const [state, dispatch] = useReducer(userReducer, initialState);
 
   const handleSignUser = async (e) => {
     e.preventDefault();
-    dispatch({ type: "RESET_ERROR" });
-    dispatch({ type: "SET_LOADING", isLoading: true });
+    dispatch({ type: 'RESET_ERROR' });
+    dispatch({ type: 'SET_LOADING', isLoading: true });
 
     const newUser = new FormData();
-    newUser.append("email", state.email);
-    newUser.append("password", state.password);
-    newUser.append("repPassword", state.repPassword);
-    newUser.append("firstName", state.firstName);
-    newUser.append("lastName", state.lastName);
-    newUser.append("nickname", state.nickname);
-    newUser.append("avatar", state.avatarImage);
+    newUser.append('email', state.email);
+    newUser.append('password', state.password);
+    newUser.append('repPassword', state.repPassword);
+    newUser.append('firstName', state.firstName);
+    newUser.append('lastName', state.lastName);
+    newUser.append('nickname', state.nickname);
+    newUser.append('avatar', state.avatarImage);
 
     try {
       const response = await axios.post(
-        import.meta.env.VITE_SERVER_URL + "/auth/signup",
+        import.meta.env.VITE_SERVER_URL + '/auth/signup',
         newUser
       );
       console.log(response.data);
-      dispatch({ type: "RESET_ERROR" });
+      dispatch({ type: 'RESET_ERROR' });
       onClose();
     } catch (error) {
       if (error?.response) {
         const responseData = error.response.data;
-        if (responseData.errors) {
-          dispatch({ type: "SET_ERROR", error: responseData.errors });
+        if (responseData.error) {
+          dispatch({ type: 'SET_ERROR', error: responseData.error });
         } else {
           if (responseData.message) {
             dispatch({
-              type: "SET_ERROR",
+              type: 'SET_ERROR',
               error: { password: responseData.message },
             });
           }
         }
-        console.log("Server error response:", responseData);
+        console.log('Server error response:', responseData);
       } else {
-        console.log("Error:", error);
+        console.log('Error:', error);
       }
     } finally {
-      dispatch({ type: "SET_LOADING", isLoading: false });
+      dispatch({ type: 'SET_LOADING', isLoading: false });
     }
   };
 
   const handleInputChange = (e) => {
     dispatch({
-      type: "SET_FIELD",
+      type: 'SET_FIELD',
       field: e.target.name,
       value: e.target.value,
     });
@@ -69,114 +69,119 @@ const Signup = ({ openSignup, onClose }) => {
       <Dialog open={openSignup} onClose={onClose}>
         <DialogTitle>SignUp</DialogTitle>
         <DialogContent>
+          {typeof state.error === 'string' && (
+            <div className='error'>
+              <p>{state.error}</p>
+            </div>
+          )}
           <UploadImage
             setAvatarImage={(image) =>
               dispatch({
-                type: "SET_FIELD",
-                field: "avatarImage",
+                type: 'SET_FIELD',
+                field: 'avatarImage',
                 value: image,
               })
             }
           />
           <TextField
             autoFocus
-            margin="dense"
-            label="Email Address"
-            name="email"
-            type="email"
+            margin='dense'
+            label='Email Address'
+            name='email'
+            type='email'
             fullWidth
-            variant="standard"
+            variant='standard'
             onChange={handleInputChange}
             required
             value={state.email}
           />
           {state.error.email && (
-            <div className="error">
+            <div className='error'>
               <p>{state.error.email}</p>
             </div>
           )}
           <TextField
             autoFocus
-            margin="dense"
-            label="Password"
-            name="password"
-            type="password"
+            margin='dense'
+            label='Password'
+            name='password'
+            type='password'
             fullWidth
-            variant="standard"
+            variant='standard'
             onChange={handleInputChange}
             required
             value={state.password}
           />
           {state.error.password && (
-            <div className="error">
+            <div className='error'>
               <p>{state.error.password}</p>
             </div>
           )}
           <TextField
             autoFocus
-            margin="dense"
-            label="Confirm password"
-            name="repPassword"
-            type="password"
+            margin='dense'
+            label='Confirm password'
+            name='repPassword'
+            type='password'
             fullWidth
-            variant="standard"
+            variant='standard'
             onChange={handleInputChange}
             required
             value={state.repPassword}
           />
           {state.error.repPassword && (
-            <div className="error">
+            <div className='error'>
               <p>{state.error.repPassword}</p>
             </div>
           )}
           <TextField
             autoFocus
-            margin="dense"
-            label="Name"
-            name="firstName"
-            type="text"
+            margin='dense'
+            label='Name'
+            name='firstName'
+            type='text'
             fullWidth
-            variant="standard"
+            variant='standard'
             onChange={handleInputChange}
             required
             value={state.firstName}
           />
           {state.error.firstName && (
-            <div className="error">
+            <div className='error'>
               <p>{state.error.firstName}</p>
             </div>
           )}
           <TextField
             autoFocus
-            margin="dense"
-            label="Last name"
-            name="lastName"
-            type="text"
+            margin='dense'
+            label='Last name'
+            name='lastName'
+            type='text'
             fullWidth
-            variant="standard"
+            variant='standard'
             onChange={handleInputChange}
             required
             value={state.lastName}
           />
           {state.error.lastName && (
-            <div className="error">
+            <div className='error'>
               <p>{state.error.lastName}</p>
             </div>
           )}
           <TextField
             autoFocus
-            margin="dense"
-            label="Nick name"
-            name="nickname"
-            type="text"
+            margin='dense'
+            label='Nick name'
+            name='nickname'
+            type='text'
             fullWidth
-            variant="standard"
+            variant='standard'
             onChange={handleInputChange}
             required
             value={state.nickname}
           />
           {state.error.nickname && (
-            <div className="error">
+            <div className='error'>
               <p>{state.error.nickname}</p>
             </div>
           )}
@@ -188,7 +193,7 @@ const Signup = ({ openSignup, onClose }) => {
             onClose={onClose}
             disabled={state.isLoading}
           >
-            {state.isLoading ? "Signing Up..." : "Sign Up"}
+            {state.isLoading ? 'Signing Up...' : 'Sign Up'}
           </Button>
         </DialogActions>
       </Dialog>
